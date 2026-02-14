e419971b208219bc4a1613a405bafc32bd73df84
Aarya Patel <aarya.patel@gmail.com>
2026-02-14 00:51:05 -0500

    Use XDG autostart instead of systemd

--- nix/home-manager-module/default.nix
+++ nix/home-manager-module/default.nix
@@ -8,45 +8,16 @@ flake: {
 in {
   options.services.simple-wallpaper-engine = {
     enable = lib.mkEnableOption "Wallpaper service (user systemd)";
-
-    # Optional: make it wait for your compositor / session bits.
-    after = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = ["graphical-session.target"];
-      description = "systemd units to start after.";
-    };
-
-    wants = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = ["graphical-session.target"];
-      description = "systemd units to pull in.";
-    };
+    xdg-autostart = lib.mkEnableOption "Wallpaper service (user systemd)";
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.user.services.simpl-wallpaper-engine = {
-      Unit = {
-        Description = "Set wallpaper";
-        After = cfg.after;
-        Wants = cfg.wants;
-        PartOf = ["graphical-session.target"];
-      };
-
-      Service = {
-        Type = "simple";
-        ExecStart = "${lib.getExe flake.packages.${pkgs.stdenv.hostPlatform.system}.default} --background";
-
-        Restart = "always";
-        RestartSec = 10;
-
-        Environment = [
-          "XDG_RUNTIME_DIR=%t"
-        ];
-      };
-
-      Install = {
-        WantedBy = ["graphical-session.target"];
-      };
+    cfg.xdg-autostart = lib.mkDefault true;
+    xdg.autostart = {
+      enable = lib.mkDefault true;
+      entries = [
+        "${flake.packages.${pkgs.stdenv.hostPlatform.system}.default}/share/applications/simple-wallpaper-engine.desktop"
+      ];
     };
   };
 }
